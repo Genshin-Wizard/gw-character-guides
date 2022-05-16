444bff0947616e33cedabf8fd9cdaaa78f9ca014
hattvr <hatterhg@gmail.com>
2022-05-16 02:34:18 -0400

    might regret this

--- update.py
+++ update.py
@@ -10,22 +10,26 @@ def format_guide(guides_dict: dict):
         if 'Set' in field['name']:
             try:
                 sets = field['value'].split('*')
-                test = sets[1].replace(' ','').replace('\n',' ').replace('<','|').replace('>', '|').lower().split('|')
-                print(test)
-                description = ''
-
+                test = sets[1].replace('\n','').replace(' ','').replace('-','').replace("'",'').replace('\n',' ').replace('<','|').replace('>', '|').lower().split('|')
                 
+                if '2x' in test[1]:
+                    description = f'x2-{test[0]}|x2-{test[2]}'
+                else:
+                    description = f'x4-{test[0]}'
+
                 field['value'] = description
             except:
                 continue
     return guides_dict
     
-# rootdir = '/Users/zaeem/Desktop/Character Guides/'
-# for file in os.listdir(rootdir):
-#     d = os.path.join(rootdir, file)
-#     if os.path.isdir(d):
-#         # path = d + '/build.json'
-#         path = 'data.json'
-#         embed_dict = json.load(open(path))
-#         test = format_guide(embed_dict)
-test = format_guide(data)
+rootdir = '/Users/zaeem/Desktop/Character Guides/'
+for file in os.listdir(rootdir):
+    if file == '.git':
+        continue
+    d = os.path.join(rootdir, file)
+    if os.path.isdir(d):
+        path = d + '/build.json'
+        embed_dict = json.load(open(path))
+        data = format_guide(embed_dict)
+        with open(f'{file}/build.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
